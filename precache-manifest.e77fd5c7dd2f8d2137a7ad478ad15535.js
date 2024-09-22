35adc0c944ac20d7680d49a41a369f2fb5b16cc9
Muhammad-Umar2000 <umar3264@gmail.com>
2024-09-23 00:58:38 +0500

    Updates

---
+++ precache-manifest.e77fd5c7dd2f8d2137a7ad478ad15535.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2896942359ac6633db7e365866251ed1",
+    "url": "/textutils/index.html"
+  },
+  {
+    "revision": "adb1463c8a8b84e2cff9",
+    "url": "/textutils/static/css/main.f4d1c333.chunk.css"
+  },
+  {
+    "revision": "0da3a3a7caac8e2de5c7",
+    "url": "/textutils/static/js/2.a5b0c296.chunk.js"
+  },
+  {
+    "revision": "60f6bf9e100e456690e9ab6c9a37bfc2",
+    "url": "/textutils/static/js/2.a5b0c296.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a8acca07efd9b80fc433",
+    "url": "/textutils/static/js/3.f665a10a.chunk.js"
+  },
+  {
+    "revision": "adb1463c8a8b84e2cff9",
+    "url": "/textutils/static/js/main.3a993a90.chunk.js"
+  },
+  {
+    "revision": "ac739b73655c13b27f74",
+    "url": "/textutils/static/js/runtime-main.be71afa4.js"
+  }
+]);
